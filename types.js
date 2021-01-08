1da39ecbaf3cd8c1f08bdccc05d77fba8f260c24
Sarah Piot <sfg.piot@outlook.com>
2021-01-08 12:05:43 -0600

    Added User type and Character type

--- types.js
+++ types.js
@@ -1,1 +1,13 @@
 // @ts-check
+/**
+ * @typedef {Object} User
+ * @property {number} id
+ * @property {string} username
+ * @property {string} [password]
+ */
+
+ /**
+  * @typedef {Object} Character
+  * @property {number} id
+  * @property {string} name
+  */
